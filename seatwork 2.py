d38260697983ba335061a40e34d0070e02da67a3
ivonne.lasco <92631714+Ivonnelasco01@users.noreply.github.com>
2022-11-18 21:35:12 +0800

    Update seatwork 2.py
    updated seatwork 2

--- seatwork 2.py
+++ seatwork 2.py
@@ -1,12 +1,5 @@
 #dictionary
-info = {
-    "Full Name":"Ivonne Glynn P. Lasco",
-    "Age":"20",
-    "Address":"Luzon Avenue",
-    "Contact Number":"09123456789",
-    "Hobby":"Programming",
-    "Course":"BS Comp. Eng.",
-}
+info = {}
 #MENU
 def menu():
     print("======== MENU ========")
@@ -16,25 +9,99 @@ def menu():
     print("4. Exit")
     print("======================")
 menu()
+def answer():
+    print("==== Add an info ====")
+    print("5. Full Name")
+    print("6. Age")
+    print("7. Address")
+    print("8. Contact Number")
+    print("9. Hobby")
+    print("10. Course")
+    print("11. Back to menu")
 while True:
     #input
     select = input("What do you want to do? >> ")
 #1. Add an info
     if select == "1":
-        #display dictionary
         print()
-        print("== Here is the info ==")
-        for key, value in info.items():
-            print(key, ":",value)
-        print()
+        answer()
         #What do you want to add?
-        add = input("What do you want to add? ")
-        input_add = input("Fill up your info: " +add+ " >> ")
-        #add to dictionary
-        info[add] = input_add
-        print()
-        print("Your info has been added.")
-        menu()
+        add = input("What do you want to add? select from (5-10) ")
+        if add == "5":
+            fullname = input("What is your full name? ")
+            info["Full name"] = fullname 
+            print("Your info has been added")
+            back = input("Do you want to add more? y/n ")
+            if back == "y":
+                print()
+                answer()
+                add = input("What do you want to add? select from (5-10) ")
+            if back == "n":
+                print()
+                menu()
+        if add == "6":
+            age = input("What is your age? ")
+            info["Age"] = age
+            print("Your info has been added")
+            back = input("Do you want to add more? y/n ")
+            if back == "y":
+                print()
+                answer()
+                add = input("What do you want to add? select from (5-10) ")
+            if back == "n":
+                print()
+                menu()
+        if add == "7":
+            address = input("What is your address? ")
+            print("Your info has been added")
+            info["Address"] = address
+            back = input("Do you want to add more? y/n ")
+            if back == "y":
+                print()
+                answer()
+                add = input("What do you want to add? select from (5-10) ")
+            if back == "n":
+                print()
+                menu()
+        if add == "8":
+            contact = input("What is your contact number? ")
+            info["Contact Number"] = contact
+            print("Your info has been added")
+            back = input("Do you want to add more? y/n ")
+            if back == "y":
+                print()
+                answer()
+                add = input("What do you want to add? select from (5-10) ")
+            if back == "n":
+                print()
+                menu()
+        if add == "9":
+            hobby = input("What is your hobby? ")
+            info["Hobby"] = hobby
+            print("Your info has been added")
+            back = input("Do you want to add more? y/n ")
+            if back == "y":
+                print()
+                answer()
+                add = input("What do you want to add? select from (5-10) ")
+            if back == "n":
+                print()
+                menu()
+        if add == "10":
+            course = input("What is your course? ")
+            info["Course"] = course
+            print("Your info has been added")
+            back = input("Do you want to add more? y/n ")
+            if back == "y":
+                print()
+                answer()
+                add = input("What do you want to add? select from (5-10) ")
+            if back == "n":
+                print()
+                menu()
+        if add == "11":
+            print()
+            menu()
 #2. Display info
     if select == "2":
         print()
